4cc126d45cf35050778100fd559bf8bb1535de13
Michael Schade <michaelschade@sddomain.com>
2010-08-14 02:23:06 -0500

    Added dynamic language lists, abstracted API call functionality, began implementation of error handling.

--- chrome/content/lifter.js
+++ chrome/content/lifter.js
@@ -7,65 +7,89 @@ if ("undefined" == typeof(Charlifter)) {
 };
 
 Charlifter.Lifter = {
+
+    codes : {
+          lang-list-outdated : 100
+        , lang-list-current  : 200
+        , lang-list-overnew  : 400
+    }
+
     init : function() {
         var contextMenu = document.getElementById("contentAreaContextMenu");
         contextMenu.addEventListener("popupshowing", this.readyContextMenu, false);
         this.getLangs(function(aEvent) {
-            this.langs = JSON.parse(aEvent.target.responseText).text.split("\n");
-            var langsMenu   = document.getElementById("charlifter-cmenu-languages-item");
-            for (lang in this.langs) {
-                let ele = langsMenu.appendItem(this.langs[lang], this.langs[lang]);
-                ele.setAttribute("oncommand", "Charlifter.Lifter.liftSelection('" + this.langs[lang] + "');");
+            response = JSON.parse(aEvent.target.responseText)
+            switch(response.code) {
+                case this.codes.lang-list-outdated:
+                    let langs = response.text.split('\n').split(':'); // [["es", "Espanol"], ["fr", "Francois"]]
+                    let langsMenu = document.getElementById("charlifter-cmenu-languages-item");
+                    for (langPair in langs) {
+                        let ele = langsMenu.appendItem(langPair[0], langPair[1]);
+                        ele.setAttribute("oncommand", String.format(
+                            "Charlifter.Lifter.liftSelection('%s');", langPair[0]
+                        ));
+                    }
+                    break;
+                case this.codes.lang-list-current:
+                    break;
+                case this.codes.lang-list-overnew:
+                    break;
+                default:
+                    break;
             }
+        }, function(aError) {
+            window.alert("Failure with call: " + request._call);
         });
     },
 
-    genRequest : function(args) {
-        var url = "http://home.mschade.me:8042/api/request/";
+    readyContextMenu : function(aE) {
+        var lift            = document.getElementById("charlifter-cmenu-item-lift");
+        var langsItem       = document.getElementById("charlifter-cmenu-languages-item");
+        lift.hidden         = !(gContextMenu.onTextInput);
+        langsItem.hidden    = !(gContextMenu.onTextInput);
+    },
+
+    genRequest : function(args, success, error) {
+        var url = "http://ares:1932/";
         var request = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
         request.open("POST", url, true);
+        request.onload  = success;
+        request.onerror = error;
         request.setRequestHeader("Content-ype", "application/json");
-        request._call = "js=" + JSON.stringify(args);
+        request.setRequestHeader("charset", "UTF-8");
+        request._call = JSON.stringify(args);
         return request;
     },
 
-    getLangs : function(success) {
+    getLangs : function(success, error) {
+        let prefs = Components.classes["@mozilla.org/preferences-service;1"]
+            .getService(Components.interfaces.nsIPrefService);
+        let locale = prefs.getBranch("general.useragent.").getCharPref("locale");
         request = this.genRequest({
-              call: "charlifter.list-languages"
-        });
-        request.onload = success;
-        request.onerror = function(aEvent) {
-            window.alert("Failure in getLangs with call: " + request._call);
-        };
+              call:     "charlifter.langs"
+            , version:  0       // TODO: Use in preferences
+            , locale:   locale
+        }, success, error);
         request.send(request._call);
     },
 
-    lift : function(lang, text, success) {
+    lift : function(lang, text, success, error) {
         request = this.genRequest({
               call: "charlifter.lift"
             , lang: lang
             , text: text
-        });
-        request.onload = success;
-        request.onerror = function(aEvent) {
-            window.alert("Failure in lift with call: " + request._call);
-        };
+        }, success, error);
         request.send(request._call);
     },
 
     liftSelection : function(lang) {
         var focused = document.commandDispatcher.focusedElement;
         focused.disabled = true;
-        this.lift(lang, focused.value, function(aEvent) {
-            focused.value       = JSON.parse(aEvent.target.responseText).text;
+        this.lift(lang, focused.value, function(aSuccess) {
+            focused.value       = JSON.parse(aSuccess.target.responseText).text;
             focused.disabled    = false;
+        }, function(aError) {
+            window.alert("Failure with call: " + request._call);
         });
-    },
-
-    readyContextMenu : function(aE) {
-        var lift        = document.getElementById("charlifter-cmenu-item-lift");
-        var langsItem   = document.getElementById("charlifter-cmenu-languages-item");
-        lift.hidden         = !(gContextMenu.onTextInput);
-        langsItem.hidden    = !(gContextMenu.onTextInput);
     },
 };
