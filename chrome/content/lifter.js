ba839c9cc06b085dc126187596a83ce927cc741d
Michael Schade <michaelschade@sddomain.com>
2010-08-27 23:09:52 -0500

    Added official GPL licensing information, updated install.rdf and creator information to reflect Spearhead Development, L.L.C.'s active participation in this project.

--- chrome/content/lifter.js
+++ chrome/content/lifter.js
@@ -1,3 +1,22 @@
+/*
+    Copyright 2010 Spearhead Development, L.L.C. <http://www.sddomain.com/>
+    
+    This file is part of Accentuate.us.
+    
+    Accentuate.us is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Accentuate.us is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with Accentuate.us. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 if ("undefined" == typeof(Charlifter)) {
     var Charlifter = {};
 };
