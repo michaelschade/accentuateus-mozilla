d48beefeff0fd0c0860b489c44c8c13422c928b3
Michael Schade <michaelschade@sddomain.com>
2010-09-19 14:50:13 -0500

    Restructured directory to include bin/ in repository.

---
+++ src/chrome/defaults/preferences/defaults.js
@@ -0,0 +1,23 @@
+/*
+    Copyright 2010 Spearhead Development, L.L.C. <http://www.sddomain.com/>
+    
+    This file is part of Accentuate.us.
+    
+    Accentuate.us is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Accentuate.us is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with Accentuate.us. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+pref('charlifter.languages.version',                0);
+pref('charlifter.languages.locale',                 'en-US');
+pref('charlifter.languages.selection-code',         'es');
+pref('charlifter.languages.feedback-success',       false);
