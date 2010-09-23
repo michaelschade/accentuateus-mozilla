7ce46e2ca9e08c6061a2527f1d96f89462942d0c
Kevin Scannell <kscanne@gmail.com>
2010-09-22 22:53:41 -0500

    Making localization check a bit more robust.

--- src/tests/locale.sh
+++ src/tests/locale.sh
@@ -1,2 +1,8 @@
 #! /bin/sh
-egrep -o '&charlifter[^;]+;' ./chrome/content/lifter.xul | sed 's/^&//;s/;$//' | while read x; do egrep -L $x ./chrome/locale/*/lifter.dtd; done |sort -u
+egrep -o '&charlifter[^;]+;' ./chrome/content/lifter.xul | sed 's/^&//;s/;$//' | while read x; do egrep -L $x ./chrome/locale/*/lifter.dtd; done |sort -u |
+if egrep 'lifter' 
+then
+	exit 1
+else
+	exit 0
+fi
